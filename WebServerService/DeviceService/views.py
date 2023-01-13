75df290c10ad9a50e280e1b0fac91090f7f61423
amahiner7 <amahiner@gmail.com>
2023-01-13 16:17:48 +0900

    1. 파일 이미지 업로드 성공시 rcode:0 반환 2. fileupload app 추가

--- WebServerService/DeviceService/views.py
+++ WebServerService/DeviceService/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import FileUploadForm
 # from .forms import ProductForm
@@ -9,7 +9,15 @@ def index(request):
 
 
 def fileupload(request):
-    if request.method == 'POST':
+    if request.method == "GET":
+        fileuploadForm = FileUploadForm
+        context = \
+        {
+            'fileuploadForm': fileuploadForm,
+        }
+        return render(request, 'DeviceService/fileupload.html', context)
+
+    elif request.method == 'POST':
         title = request.POST['title']
         imgfile = request.FILES["imgfile"]
         fileupload = FileUpload(
@@ -17,13 +25,9 @@ def fileupload(request):
             imgfile=imgfile,
         )
         fileupload.save()
-        return redirect('fileupload')
-    else:
-        fileuploadForm = FileUploadForm
-        context = {
-            'fileuploadForm': fileuploadForm,
-        }
-        return render(request, 'DeviceService/fileupload.html', context)
+
+        result = {"rCode": "0", "rMessage": "Success"}
+        return JsonResponse(result, safe=False)
 
 
 # def product_create(request):
