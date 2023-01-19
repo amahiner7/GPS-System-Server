bc620d588d2fbc1f2ba5224a1f5dda53dffb6a6a
amahiner7 <amahiner@gmail.com>
2023-01-19 17:18:37 +0900

    display_gps_score / display_par_data 분리

--- DataService/database_service.py
+++ DataService/database_service.py
@@ -82,8 +82,63 @@ class DatabaseService:
               "PAR_CNT_01, PAR_CNT_02, PAR_CNT_03, PAR_CNT_04, PAR_CNT_05, " \
               "PAR_CNT_06, PAR_CNT_07, PAR_CNT_08, PAR_CNT_09 " \
               "FROM GA0200 " \
-              f"WHERE CO_DIV = {co_div} " \
+              f"WHERE CO_DIV = '{co_div}' " \
               "ORDER BY COUR_CD; "
+        try:
+            self.db_cursor.execute(sql)
+        except Exception as ex:
+            print("DatabaseService.get_par_data():", ex)
+            return {'resultCode': 500, 'resultMsg': 'query execution fail.'}
+
+        results = [dict((self.db_cursor.description[i][0], value) for i, value in enumerate(row.values()))
+                   for row in self.db_cursor.fetchall()]
+
+        if results is not None and len(results) > 0:
+            return results
+        else:
+            return None
+
+    def get_gps_score_data(self, co_div, game_sid):
+        sql = "SELECT G.COUR_CD_P, G.GAME_SID, G.CHANGE_CD_P, G.CHANGE_CD_N, G.CHANGE_CD_A, P.CHKIN_NO, P.CUST_NM, " \
+              "CASE WHEN S.COURSE_A IS NULL THEN G.CHANGE_CD_P ELSE S.COURSE_A END COURSE_A, " \
+              "CASE WHEN S.SCORE_A1 IS NULL THEN 0 ELSE S.SCORE_A1 END SCORE_A1, " \
+              "CASE WHEN S.SCORE_A2 IS NULL THEN 0 ELSE S.SCORE_A2 END SCORE_A2, " \
+              "CASE WHEN S.SCORE_A3 IS NULL THEN 0 ELSE S.SCORE_A3 END SCORE_A3, " \
+              "CASE WHEN S.SCORE_A4 IS NULL THEN 0 ELSE S.SCORE_A4 END SCORE_A4, " \
+              "CASE WHEN S.SCORE_A5 IS NULL THEN 0 ELSE S.SCORE_A5 END SCORE_A5, " \
+              "CASE WHEN S.SCORE_A6 IS NULL THEN 0 ELSE S.SCORE_A6 END SCORE_A6, " \
+              "CASE WHEN S.SCORE_A7 IS NULL THEN 0 ELSE S.SCORE_A7 END SCORE_A7, " \
+              "CASE WHEN S.SCORE_A8 IS NULL THEN 0 ELSE S.SCORE_A8 END SCORE_A8, " \
+              "CASE WHEN S.SCORE_A9 IS NULL THEN 0 ELSE S.SCORE_A9 END SCORE_A9, " \
+              "CASE WHEN S.COURSE_B IS NULL THEN G.CHANGE_CD_N ELSE S.COURSE_B END COURSE_B, " \
+              "CASE WHEN S.SCORE_B1 IS NULL THEN 0 ELSE S.SCORE_B1 END SCORE_B1, " \
+              "CASE WHEN S.SCORE_B2 IS NULL THEN 0 ELSE S.SCORE_B2 END SCORE_B2, " \
+              "CASE WHEN S.SCORE_B3 IS NULL THEN 0 ELSE S.SCORE_B3 END SCORE_B3, " \
+              "CASE WHEN S.SCORE_B4 IS NULL THEN 0 ELSE S.SCORE_B4 END SCORE_B4, " \
+              "CASE WHEN S.SCORE_B5 IS NULL THEN 0 ELSE S.SCORE_B5 END SCORE_B5, " \
+              "CASE WHEN S.SCORE_B6 IS NULL THEN 0 ELSE S.SCORE_B6 END SCORE_B6, " \
+              "CASE WHEN S.SCORE_B7 IS NULL THEN 0 ELSE S.SCORE_B7 END SCORE_B7, " \
+              "CASE WHEN S.SCORE_B8 IS NULL THEN 0 ELSE S.SCORE_B8 END SCORE_B8, " \
+              "CASE WHEN S.SCORE_B9 IS NULL THEN 0 ELSE S.SCORE_B9 END SCORE_B9, " \
+              "CASE WHEN S.SCORE_TOTAL_A IS NULL THEN 0 ELSE S.SCORE_TOTAL_A END SCORE_TOTAL_A, " \
+              "CASE WHEN S.SCORE_TOTAL_B IS NULL THEN 0 ELSE S.SCORE_TOTAL_B END SCORE_TOTAL_B, " \
+              "CASE WHEN S.SCORE_TOTAL   IS NULL THEN 0 ELSE S.SCORE_TOTAL   END SCORE_TOTAL, " \
+              "G.SCORE_PIC_URL1, S.SMS_SEND , H.EN_PHONE " \
+              "FROM  GC0110 P INNER JOIN GD0100 G ON G.CO_DIV = P.CO_DIV " \
+              "AND G.GAME_DT = P.GAME_DT " \
+              "AND G.GAME_SID = P.GAME_SID " \
+              "INNER JOIN GD0300 S ON P.CO_DIV = S.CO_DIV " \
+              "AND P.GAME_DT = S.GAME_DT " \
+              "AND P.CHKIN_NO = S.CHKIN_NO " \
+              "AND S.GAME_SID = P.GAME_SID " \
+              "INNER JOIN EN_HISTORY H ON G.CO_DIV = H.CO_DIV " \
+              "AND G.GAME_DT = H.EN_DAY " \
+              "AND G.COUR_CD_P = H.EN_COS AND G.GAME_TI = H.EN_TIME and P.CHKIN_NO = H.en_chkinno " \
+              f"WHERE P.CO_DIV = '{co_div}' " \
+              "AND P.CHECKINYN = 'Y' " \
+              f"AND p.game_sid ='{game_sid}' " \
+              "AND G.DEL_YN = 'N' " \
+              "ORDER by S.EN_SEQ ASC; "
         try:
             self.db_cursor.execute(sql)
         except Exception as ex:
--- WebServerService/gps_score_app/display_gps_score_view.py
+++ WebServerService/gps_score_app/display_gps_score_view.py
@@ -7,20 +7,57 @@ from .singleton_object import SingletonObject
 
 @method_decorator(csrf_exempt, name="dispatch")
 class DisplayGPSScoreView(View):
-    def get(self, request, param):
+    def __display(self, request):
         try:
-            if param == "display":
-                co_div = request.GET["co_div"]
-                game_sid = request.GET["game_sid"]
+            co_div = request.GET["co_div"]
+            game_sid = request.GET["game_sid"]
 
-                print("co_div: ", co_div)
-                print("game_sid: ", game_sid)
-                par_data_list = SingletonObject.database_service.get_par_data(co_div)
+            print("co_div: ", co_div)
+            print("game_sid: ", game_sid)
+            par_data_list = SingletonObject.database_service.get_par_data(co_div)
 
-                context = {'par_data_list': par_data_list}
+            context = {'par_data_list': par_data_list}
 
-                return render(
-                    request, 'gps_score_app/display_gps_score.html', context)
+            return render(
+                request, 'gps_score_app/display_par_data.html', context)
         except Exception as ex:
-            print("DisplayGPSScoreView.get():", ex)
+            print("DisplayGPSScoreView.__display():", ex)
             return {'rCode': 500, 'rMessage': str(ex)}
+
+    def __display_par_data(self, request):
+        try:
+            co_div = request.GET["co_div"]
+
+            par_data_list = SingletonObject.database_service.get_par_data(co_div)
+
+            context = {'par_data_list': par_data_list}
+
+            return render(
+                request, 'gps_score_app/display_par_data.html', context)
+        except Exception as ex:
+            print("DisplayGPSScoreView.__display_par_data():", ex)
+            return {'rCode': 500, 'rMessage': str(ex)}
+
+    def __display_gps_score(self, request):
+        try:
+            co_div = request.GET["co_div"]
+            game_sid = request.GET["game_sid"]
+
+            par_data_list = SingletonObject.database_service.get_par_data(co_div)
+
+            context = {'par_data_list': par_data_list}
+
+            return render(
+                request, 'gps_score_app/display_gps_score.html', context)
+        except Exception as ex:
+            print("DisplayGPSScoreView.__display_gps_score():", ex)
+            return {'rCode': 500, 'rMessage': str(ex)}
+
+    def get(self, request, param):
+        if param == "display":
+            self.__display(request)
+        if param == "par_data":
+            self.__display_par_data(request)
+        elif param == "gps_score":
+            self.__display_gps_score(request)
+
