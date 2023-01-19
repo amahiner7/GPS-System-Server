aa333c0e6e7ba591006068ed4c797b4a58563bd4
amahiner7 <amahiner@gmail.com>
2023-01-19 10:17:10 +0900

    환경설정 추가  - configs.py 추가

---
+++ Config/configs.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def load_command_args():
+    parser = argparse.ArgumentParser(description="GPS System Server")
+    parser.add_argument("--db_ip", type=str, default=Configs.default_db_ip)
+    parser.add_argument("--db_port", type=str, default=Configs.default_db_port)
+    parser.add_argument("--db_name", type=str, default=Configs.default_db_name)
+    parser.add_argument("--db_id", type=str, default=Configs.default_db_id)
+    parser.add_argument("--db_password", type=str, default=Configs.default_db_password)
+    Configs.args = parser.parse_args()
+
+    Configs.db_ip = Configs.args.db_ip
+    Configs.db_name = Configs.args.db_name
+    Configs.db_id = Configs.args.db_id
+    Configs.db_password = Configs.args.db_password
+
+
+class Configs:
+    clubd_default_db_ip = "202.68.227.57"
+    clubd_default_db_port = 13306
+    clubd_default_db_name = "bear"
+    clubd_default_db_id = "greenit"
+    clubd_default_db_password = "greenitgps^@@*6228"
+
+    db_ip = ""
+    db_name = ""
+    db_id = ""
+    db_password = ""
+
+    args = None
