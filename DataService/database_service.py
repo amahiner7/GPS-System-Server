f9b90d73c22ed75400f07c8a386dd5f58a682dd2
amahiner7 <amahiner@gmail.com>
2023-01-19 10:10:32 +0900

    데이터 서비스 기능 추가  - 데이터베이스 서비스(database_service.py) 추가

---
+++ DataService/database_service.py
@@ -0,0 +1,70 @@
+import pymysql
+from urllib.parse import urlsplit, parse_qsl
+from datetime import datetime
+
+
+class DatabaseService:
+    def __init__(self):
+        self.db_ip = ""
+        self.db_port = 0
+        self.db_name = ""
+        self.db_id = ""
+        self.db_password = ""
+        self.db_conn = None
+        self.db_cursor = None
+
+    def __check_db_conn(self):
+        if self.db_conn is None or self.db_cursor is None:
+            self.re_connect()
+
+    def __close_db(self):
+        try:
+            if self.db_cursor is not None:
+                self.db_cursor.close()
+
+            if self.db_conn is not None:
+                self.db_conn.close()
+        except Exception as ex:
+            print("DatabaseService.__close_db():", ex)
+        finally:
+            self.db_cursor = None
+            self.db_conn = None
+
+    def connect(self, db_ip, db_port, id, password, db_name, print_msg=True):
+        self.db_ip = db_ip
+        self.db_port = db_port
+        self.db_id = id
+        self.db_password = password
+        self.db_name = db_name
+
+        try:
+            self.db_conn = pymysql.connect(
+                user=id,
+                password=password,
+                host=db_ip,
+                port=db_port,
+                db=db_name,
+                charset='utf8'
+            )
+
+            self.db_cursor = self.db_conn.cursor(pymysql.cursors.DictCursor)
+            if print_msg:
+                print(f"Connect to {self.db_ip}, {db_name} success.")
+            return True
+
+        except Exception as ex:
+            if print_msg:
+                print(f"Connect to {db_ip}, {db_name} is failed.")
+            print(str(ex))
+            return False
+
+    def re_connect(self):
+        result = self.connect(
+            db_ip=self.db_ip,
+            db_port=self.db_port,
+            id=self.db_id,
+            password=self.db_password,
+            db_name=self.db_name,
+            print_msg=False)
+
+        return result
