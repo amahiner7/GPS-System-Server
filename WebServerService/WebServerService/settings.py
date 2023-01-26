3dc0f7c4f6981ccbb5c3d7e9a99a827c7ff88637
amahiner7 <amahiner@gmail.com>
2023-01-26 14:20:41 +0900

    mac-osx에서 mysqlclient 오류 발생 수정: pymysql.install_as_MySQLdb() 적용

--- WebServerService/WebServerService/settings.py
+++ WebServerService/WebServerService/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 from . import db_setting
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- WebServerService/gps_score_app/display_gps_score_view.py
+++ WebServerService/gps_score_app/display_gps_score_view.py
@@ -11,57 +11,64 @@ class DisplayGPSScoreView(TemplateView):
     template_name = "gps_score_app/display.html"
     qeuryset = None
 
+    def get_gps_score_data(self, co_div, game_sid):
+        hole_par_A_data_list = SingletonObject.database_service.get_hole_par_data(co_div=co_div, cour_name="OUT")
+        hole_par_B_data_list = SingletonObject.database_service.get_hole_par_data(co_div=co_div, cour_name="IN")
+        gps_score_data_list = SingletonObject.database_service.get_gps_score_data(
+            co_div=co_div,
+            game_sid=game_sid)
+
+        total_Par_A = 0
+        total_Par_B = 0
+
+        for cnt_num in range(1, 10):
+            cnt_str = "PAR_CNT_0" + str(cnt_num)
+            par_cnt_A_value = int(hole_par_A_data_list[0][cnt_str])
+            par_cnt_B_value = int(hole_par_B_data_list[0][cnt_str])
+
+            total_Par_A = total_Par_A + par_cnt_A_value
+            total_Par_B = total_Par_B + par_cnt_B_value
+
+        total_Par = total_Par_A + total_Par_B
+
+        for gps_score_data in gps_score_data_list:
+            for cnt_num in range(1, 10):
+                cnt_str = "PAR_CNT_0" + str(cnt_num)
+                par_cnt_A_value = int(hole_par_A_data_list[0][cnt_str])
+                par_cnt_B_value = int(hole_par_B_data_list[0][cnt_str])
+
+                score_A_str = "SCORE_A" + str(cnt_num)
+                score_B_str = "SCORE_B" + str(cnt_num)
+                calc_score_A_str = "CALC_" + score_A_str
+                calc_score_B_str = "CALC_" + score_B_str
+
+                score_A_value = int(gps_score_data_list[0][score_A_str])
+                score_B_value = int(gps_score_data_list[0][score_B_str])
+
+                calc_score_A_value = score_A_value - par_cnt_A_value
+                calc_score_B_value = score_B_value - par_cnt_B_value
+                gps_score_data[calc_score_A_str] = calc_score_A_value
+                gps_score_data[calc_score_B_str] = calc_score_B_value
+
+        return hole_par_A_data_list, hole_par_B_data_list, gps_score_data_list, total_Par_A, total_Par_B, total_Par
+
     def get(self, request, *args, **kwargs):
         if kwargs["param"] == "display":
             try:
                 co_div = request.GET["co_div"]
                 game_sid = request.GET["game_sid"]
-                hole_par_A_data_list = SingletonObject.database_service.get_hole_par_data(co_div=co_div, cour_name="OUT")
-                hole_par_B_data_list = SingletonObject.database_service.get_hole_par_data(co_div=co_div, cour_name="IN")
-                gps_score_data_list = SingletonObject.database_service.get_gps_score_data(
-                    co_div=co_div,
-                    game_sid=game_sid)
 
-                total_Par_A = 0
-                total_Par_B = 0
-
-                for cnt_num in range(1, 10):
-                    cnt_str = "PAR_CNT_0" + str(cnt_num)
-                    par_cnt_A_value = int(hole_par_A_data_list[0][cnt_str])
-                    par_cnt_B_value = int(hole_par_B_data_list[0][cnt_str])
-
-                    total_Par_A = total_Par_A + par_cnt_A_value
-                    total_Par_B = total_Par_B + par_cnt_B_value
-
-                total_Par = total_Par_A + total_Par_B
-
-                for gps_score_data in gps_score_data_list:
-                    for cnt_num in range(1, 10):
-                        cnt_str = "PAR_CNT_0" + str(cnt_num)
-                        par_cnt_A_value = int(hole_par_A_data_list[0][cnt_str])
-                        par_cnt_B_value = int(hole_par_B_data_list[0][cnt_str])
-
-                        score_A_str = "SCORE_A" + str(cnt_num)
-                        score_B_str = "SCORE_B" + str(cnt_num)
-                        calc_score_A_str = "CALC_" + score_A_str
-                        calc_score_B_str = "CALC_" + score_B_str
-
-                        score_A_value = int(gps_score_data_list[0][score_A_str])
-                        score_B_value = int(gps_score_data_list[0][score_B_str])
-
-                        calc_score_A_value = score_A_value - par_cnt_A_value
-                        calc_score_B_value = score_B_value - par_cnt_B_value
-                        gps_score_data[calc_score_A_str] = calc_score_A_value
-                        gps_score_data[calc_score_B_str] = calc_score_B_value
+                hole_par_A_data_list, hole_par_B_data_list, gps_score_data_list, total_Par_A, total_Par_B, total_Par = \
+                    self.get_gps_score_data(co_div=co_div, game_sid=game_sid)
 
                 context = {
                     'view': self.__class__.__name__,
                     'hole_par_A_data_list': hole_par_A_data_list,
                     'hole_par_B_data_list': hole_par_B_data_list,
+                    'gps_score_data_list': gps_score_data_list,
                     'total_Par_A': str(total_Par_A),
                     'total_Par_B': str(total_Par_B),
-                    'total_Par': str(total_Par),
-                    'gps_score_data_list': gps_score_data_list,
+                    'total_Par': str(total_Par)
                 }
 
                 return render(
