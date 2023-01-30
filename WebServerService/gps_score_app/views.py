1ec7623d6acc371e1ccff66cef9e1791c60cb4af
amahiner7 <amahiner@gmail.com>
2023-01-30 14:40:42 +0900

    불필요한 소스 제거 1. display_par_data.html 2. display_gps_score.html 2. image_template.html

--- WebServerService/gps_score_app/views.py
+++ WebServerService/gps_score_app/views.py
@@ -11,27 +11,3 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 
 from Commons.common import *
-
-wkhtml_to_image = os.path.join(
-    settings.BASE_DIR, "gps_score_app/wkhtmltoimage.exe")
-
-
-def html_to_image(request):
-    template_path = 'gps_score_app/image_template.html'
-    template = get_template(template_path)
-
-    context = {"name": "Areeba Seher"}
-    html = template.render(context)
-
-    config = imgkit.config(wkhtmltoimage=wkhtml_to_image, xvfb='/opt/bin/xvfb-run')
-
-    image = imgkit.from_string(html, False, config=config)
-
-    # Generate download
-    response = HttpResponse(image, content_type='image/jpeg')
-
-    response['Content-Disposition'] = 'attachment; filename=image.jpg'
-    # print(response.status_code)
-    if response.status_code != 200:
-        return HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
